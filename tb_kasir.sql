b5f91b82ae4a2f863e3b8a8684c7417f4f718fd8
mramadhanurh <mramadhanur@gmail.com>
2022-08-27 11:43:06 +0700

    uploud project

---
+++ tb_kasir.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Apr 2022 pada 11.15
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tb_kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `id_barang` varchar(100) NOT NULL,
+  `nama_barang` text NOT NULL,
+  `harga_barang` varchar(250) NOT NULL,
+  `tgl_input` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `id_barang`, `nama_barang`, `harga_barang`, `tgl_input`) VALUES
+(23, 'BRG001', 'sosis kecil', '2000', '2 April 2022, 21:33'),
+(24, 'BRG002', 'fishroll', '2000', '2 April 2022, 21:34'),
+(25, 'BRG003', 'sosis jumbo', '5000', '2 April 2022, 21:34');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id_cart` int(11) NOT NULL,
+  `kode_barang` varchar(255) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `harga_barang` varchar(255) NOT NULL,
+  `quantity` text NOT NULL,
+  `subtotal` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  `no_transaksi` varchar(255) NOT NULL,
+  `bayar` varchar(255) NOT NULL,
+  `kembalian` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `keranjang`
+--
+
+INSERT INTO `keranjang` (`id_cart`, `kode_barang`, `nama_barang`, `harga_barang`, `quantity`, `subtotal`, `tgl_input`, `no_transaksi`, `bayar`, `kembalian`) VALUES
+(87, 'BRG001', 'sosis kecil', '2000', '2', '4000', '2 April 2022', 'AD20420222145', '300000', '14000'),
+(88, 'BRG002', 'fishroll', '2000', '6', '12000', '2 April 2022', 'AD20420222145', '300000', '14000'),
+(89, 'BRG003', 'sosis jumbo', '5000', '54', '270000', '2 April 2022', 'AD20420222145', '300000', '14000'),
+(90, 'BRG001', 'sosis kecil', '2000', '4', '8000', '23 April 2022', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `laporanku`
+--
+
+CREATE TABLE `laporanku` (
+  `id_cart` int(11) NOT NULL,
+  `kode_barang` varchar(255) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `harga_barang` varchar(255) NOT NULL,
+  `quantity` text NOT NULL,
+  `subtotal` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL,
+  `no_transaksi` varchar(255) NOT NULL,
+  `bayar` varchar(255) NOT NULL,
+  `kembalian` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `laporanku`
+--
+
+INSERT INTO `laporanku` (`id_cart`, `kode_barang`, `nama_barang`, `harga_barang`, `quantity`, `subtotal`, `tgl_input`, `no_transaksi`, `bayar`, `kembalian`) VALUES
+(85, 'BRG001', 'sosis kecil', '2000', '2', '4000', '2 April 2022', 'AD20420222136', '30000', '6000'),
+(86, 'BRG003', 'sosis jumbo', '5000', '4', '20000', '2 April 2022', 'AD20420222136', '30000', '6000');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `nama_toko` varchar(50) NOT NULL,
+  `user` varchar(250) NOT NULL,
+  `pass` varchar(250) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `telp` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id_login`, `nama_toko`, `user`, `pass`, `alamat`, `telp`) VALUES
+(1, 'KASIRKU', 'admin', '123', 'Kp. Lukun Rt.02/01 Ds. Cisoka Kec. Cisoka Kab. Tangerang', '089635808393');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_cart`);
+
+--
+-- Indeks untuk tabel `laporanku`
+--
+ALTER TABLE `laporanku`
+  ADD PRIMARY KEY (`id_cart`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+
+--
+-- AUTO_INCREMENT untuk tabel `laporanku`
+--
+ALTER TABLE `laporanku`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ template/footer.php
@@ -0,0 +1,29 @@
+</div><!-- end row -->
+</div> <!-- end container fluid -->
+
+<footer class="text-center mb-0 py-3">
+    <p class="text-muted small mb-0">Copyright &copy; <?php echo  date("Y");?> <a href="https://www.youtube.com/channel/UC-GqTlLxKeP-G6BfY9YD8MQ" style="text-decoration:none;">
+    <b>m.ramadhanur.h</b></a>. All Rights Reserved</p>
+</footer>
+
+    <script src="assets/js/jquery.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="assets/DataTables/datatables.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+    $('#table').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
+$(document).ready(function() {
+    $('#table_barang').DataTable( {
+    } );
+} );
+    </script>
+
+</body>
+</html>
